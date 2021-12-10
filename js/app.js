6180b164c245e0ee328425f31f9174ce9628658d
FherAlberto <fheralberto@gmail.com>
2021-12-10 08:07:33 -0600

    Fondos quitados, estilos al range, fondo 2k

--- js/app.js
+++ js/app.js
@@ -1,32 +1,39 @@
 window.addEventListener('load', cargaFiguras)
 function cargaFiguras() {
   // Cochinito
-  slider01 = document.querySelector('#slider01');
-  canvas01 = document.querySelector('#canvas01')
-  ctx01 = canvas01.getContext('2d');
+  const slider01 = document.querySelector('#slider01');
+  const canvas01 = document.querySelector('#canvas01')
+  const ctx01 = canvas01.getContext('2d');
   const images01 = [];
-  pageLoaded(slider01, canvas01, '01-cochinito', ctx01, images01);
+  const tipo01 = '.png';
+  pageLoaded(slider01, canvas01, '01-cochinito', ctx01, images01, tipo01);
   // Patito
-  slider02 = document.querySelector('#slider02');
-  canvas02 = document.querySelector('#canvas02')
-  ctx02 = canvas02.getContext('2d');
+  const slider02 = document.querySelector('#slider02');
+  const canvas02 = document.querySelector('#canvas02')
+  const ctx02 = canvas02.getContext('2d');
   const images02 = [];
-  pageLoaded(slider02, canvas02, '02-patito', ctx02, images02);
+  const tipo02 = '.png';
+  pageLoaded(slider02, canvas02, '02-patito', ctx02, images02, tipo02);
   // Reno Verde
-  slider03 = document.querySelector('#slider03');
-  canvas03 = document.querySelector('#canvas03')
-  ctx03 = canvas03.getContext('2d');
+  const slider03 = document.querySelector('#slider03');
+  const canvas03 = document.querySelector('#canvas03')
+  const ctx03 = canvas03.getContext('2d');
   const images03 = [];
-  pageLoaded(slider03, canvas03, '03-renoVerde', ctx03, images03);
+  const tipo03 = '.png';
+  pageLoaded(slider03, canvas03, '03-renoVerde', ctx03, images03, tipo03);
 };
-function pageLoaded(sliderInput, canvas, carpeta, ctx, images) {
+function pageLoaded(sliderInput, canvas, carpeta, ctx, images, tipo) {
   sliderInput.addEventListener('input', () => {
     handleInputSlider(sliderInput, ctx, images, canvas)
   });
-  for (let i = 1; i <= 36; i += 1) {
+  for (let i = 1; i <= 37; i += 1) {
     const number = i.toString().padStart(2, '00');
     const image = new Image()
-    image.src = `../img/${carpeta}/${number}.jpg`
+    if(i === 37){
+      image.src = `../img/${carpeta}/01${tipo}`  
+    } else {
+      image.src = `../img/${carpeta}/${number + tipo}`
+    }
     image.addEventListener('load', () => {
       images[i] = image
       if (i === 1) {
@@ -36,6 +43,7 @@ function pageLoaded(sliderInput, canvas, carpeta, ctx, images) {
   }
 }
 function loadImage(ctx, images, canvas) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(images, 0, 0, canvas.width, canvas.height);
 }
 function handleInputSlider(sliderInput, ctx, images, canvas) {
