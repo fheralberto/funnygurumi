03827165fdbae7fc2b0c8d8efd708c72464b93a3
FherAlberto <fheralberto@gmail.com>
2022-02-02 03:51:14 -0600

    SEO, galería minificada y aleatoria

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   iniciar();
-  // setTimeout(()=>{
-  //   colorearLogo();
-  // }, 2500);
+  setTimeout(()=>{
+    colorearLogo();
+  }, 3000);
 });
 // Variables globales-------------------------
 const main = document.querySelector('#main');
@@ -108,19 +108,34 @@ function queriesListeners(origen) {
   hijos = galeria.childNodes.length
 }
 
+function aleatorioEntero(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 function mostrarGaleria(max) {
   limpiaGaleria();
   // console.log('Mostrando ', max);
+
+  let lista = [];
+  for (let i = 0; i < max; i++) {
+    do{
+      aleatorio = aleatorioEntero(0, 14);
+    }
+    while(lista.includes(aleatorio));
+    lista.push(aleatorio);
+  }
+  // console.log(lista);
   for (let i = 0; i < max; i++) {
     const galeriaItem = document.createElement('div');
     galeriaItem.classList.add('galeria_item');
 
+
     const imagenItem = document.createElement('img');
-    imagenItem.setAttribute("src", `img/galeria/${datosGaleria[i].nombre}.png`);
-    imagenItem.setAttribute("alt", `${datosGaleria[i].alt}`);
+    imagenItem.setAttribute("src", `img/galeria/${datosGaleria[lista[i]].nombre}.png`);
+    imagenItem.setAttribute("alt", `${datosGaleria[lista[i]].alt}`);
 
     //Le crea atributo data-imagen-id = "i" 
-    imagenItem.dataset.imagenId = datosGaleria[i].nombre;
+    imagenItem.dataset.imagenId = datosGaleria[lista[i]].nombre;
     // Se le agrega el evento onclick ------------------------------------------
     imagenItem.onclick = ampliarImagen;
 
@@ -163,7 +178,10 @@ function mostrarTodos() {
 
     const autor = document.createElement('div');
     autor.classList.add('autor');
-    autor.textContent = `Autor: ${imagen.autor}`
+    autor.textContent = 'by ';
+    const span = document.createElement('span');
+    span.textContent = imagen.autor;
+    autor.appendChild(span);
 
     const descripcion = document.createElement('div');
     descripcion.classList.add('descripcion');
